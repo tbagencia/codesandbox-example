9d0e345d3b8eeec8bfe31238456791bba8dd274d
Nicolás Cubero <nicocubero@gmail.com>
2025-04-04 14:32:40 +0000

    Cambio de título

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Culo de Burro</h1>
+      <h1>Hello Amigos del Rojo!</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
